8922eceaec14fe633908aa09f478df4838d58c55
matheus-araujop <matheusp.araujo0@gmail.com>
2021-05-25 19:20:25 -0300

    Adicionado desafio 03

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let n = 1 
+
+for(n ; n <=10; n++){
+    console.log(n)
+}
+
+// Exercício 1
+
+while(n <=10 ){
+    console.log(n);
+    n ++;
+}
+
+// Exercício 2
+
+let j = 1 // primeiro número ímpar
+let i = 1 // contador
+
+for(j; j < 100; j = j + 2){
+
+    console.log(i);
+    i = i + 1;
+    
+}
+
+// Exercício 3 (conta ímpares)
+
+let k = 0 // primeiro número par
+let g = 1 // contador
+
+for(k; k < 100; k = k + 2){
+
+    console.log(g);
+    g = g + 1;
+    
+}
+
+// Exercício 3 (conta pares)
